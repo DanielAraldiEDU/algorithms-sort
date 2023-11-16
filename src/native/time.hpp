d30b99daa94e30a3773e8b5a20aed0400cbef67f
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2023-11-16 16:45:13 -0300

    fix: added time to calculate media

--- src/native/time.hpp
+++ src/native/time.hpp
@@ -1,21 +1,17 @@
 #include <iostream>
-#include <chrono>
 
 void getTime(char type, int array[], int repeats, int length, char option) {
   double allTime = 0;
   for (int index = 0; index < repeats; index++) {
-    const auto startTime = chrono::high_resolution_clock::now();
-    
+    int start = time(NULL);
+
     if (type == 'Q') handleQuickSort(array, 0, length - 1, option);
     else handleShellSort(array, length - 1, option);
     
-    const auto endTime = chrono::high_resolution_clock::now();
-
-    chrono::duration<double> time = endTime - startTime;
-    cout << "Time: " << time.count() << endl;
-    allTime += time.count();
+    int end = time(NULL);
+    allTime += double(end - start);
   }
 
-  const double media = allTime/repeats;
+  const double media = allTime / repeats;
   cout << "Media time executed: " << media << endl;
 }

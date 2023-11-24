971aaa0ed5334b95d7d2ba3ea8f8dfbaf10faf79
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-11-24 11:34:06 -0300

    feat: using `clock_t` to calculate execution time

--- src/native/time.hpp
+++ src/native/time.hpp
@@ -1,17 +1,29 @@
 #include <iostream>
 
-void getTime(char type, int array[], int repeats, int length, char option) {
+void getTime(char type, int array[], int repeats, int length, char option, char typeArray) {
   double allTime = 0;
+  
+  clock_t start, end;
   for (int index = 0; index < repeats; index++) {
-    int start = time(NULL);
+    if(typeArray == 'B') generatePoorCase(array, length);
+    else if(typeArray == 'G') generateGoodCase(array, length);
+    else generateRandomCase(array, length);
+
+    start = clock();
 
     if (type == 'Q') handleQuickSort(array, 0, length - 1, option);
     else handleShellSort(array, length - 1, option);
-    
-    int end = time(NULL);
-    allTime += double(end - start);
+
+    end = clock();
+
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+
+    cout << "time_taken: ("<<index<<") = " << time_taken << endl;
+    allTime += time_taken;
   }
 
-  const double media = allTime / repeats;
+  cout << "ALL TIME: " << allTime << endl;
+
+  double media = allTime / repeats;
   cout << "Media time executed: " << media << endl;
 }
